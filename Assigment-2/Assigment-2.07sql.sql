41c928a0ff44609cf05bfb1a7d4a843be1d91cc6
Mayank sharma <126330546+Mayankaharma2003@users.noreply.github.com>
2025-02-12 15:35:55 +0530

    ,

--- Assigment-2/Assigment-2.07sql.sql
+++ Assigment-2/Assigment-2.07sql.sql
@@ -1,8 +1,5 @@
-SELECT 
-oh.ORDER_ID
-,oh.STATUS_ID
-,f.FACILITY_ID
-,f.FACILITY_NAME
-,f.FACILITY_TYPE_ID
-,ft.PARENT_TYPE_ID
-from order_header oh join order_item oi on oi.order_id = oh.ORDER_ID join product p on p.PRODUCT_ID=oi.PRODUCT_ID join facility f on f.FACILITY_ID=p.FACILITY_ID join facility_type ft on ft.FACILITY_TYPE_ID=f.FACILITY_TYPE_ID;
+SELECT oi.ORDER_ID,oi.STATUS_ID,pf.FACILITY_ID,f.FACILITY_NAME,f.FACILITY_TYPE_ID,ft.PARENT_TYPE_ID
+from order_item oi 
+join product_facility pf on pf.PRODUCT_ID=oi.PRODUCT_ID 
+join facility f on f.FACILITY_ID=pf.FACILITY_ID 
+join facility_type ft on ft.FACILITY_TYPE_ID=f.FACILITY_TYPE_ID;

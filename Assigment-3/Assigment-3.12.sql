4f13c5a1b2016b3627d1b9a6bb49c1a3ca6a4e69
Mayank sharma <126330546+Mayankaharma2003@users.noreply.github.com>
2025-02-13 19:38:25 +0530

    Assigment-3 question - 12.1

--- Assigment-3/Assigment-3.12.sql
+++ Assigment-3/Assigment-3.12.sql
@@ -2,5 +2,6 @@ select oh.Order_id ,
 	   oisg.facility_id,
        oh.order_date,
        oh.status_id,
+       pl.STATUS_ID,
        datediff(date(oh.entry_date),date(oh.order_Date)) as duration
-from order_header oh join order_item_ship_group oisg on oisg.ORDER_ID = oh.ORDER_ID;       
+from order_header oh join order_item_ship_group oisg on oisg.ORDER_ID = oh.ORDER_ID join picklist pl on pl.FACILITY_ID=oisg.FACILITY_ID and pl.STATUS_ID is null;       
